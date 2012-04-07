c837970a7cc6d07583baf8c306c2a7c5c67293fc
Edson <edonle@gmail.com>
2012-04-06 22:51:38 -0300

    primeiro commit do HelloTabWidget

---
+++ HelloTabWidget/src/hello/tab/widget/HelloTabWidgetActivity.java
@@ -0,0 +1,13 @@
+package hello.tab.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloTabWidgetActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
